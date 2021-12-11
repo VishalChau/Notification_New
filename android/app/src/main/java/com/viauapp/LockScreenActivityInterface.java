3d8cb5af727b79bd76bcf55a5e346d8e1e008798
vchauhan <vishalchau1997@gmail.com>
2021-12-11 11:21:17 +0530

    Visitor Pass Notifications

---
+++ android/app/src/main/java/com/viauapp/LockScreenActivityInterface.java
@@ -0,0 +1,11 @@
+package com.stouch;
+
+import com.facebook.react.bridge.ReadableMap;
+
+public interface LockScreenActivityInterface {
+    public void onConnected();
+
+    public void onConnectFailure();
+
+    public void onIncoming(ReadableMap params);
+}
